5ffe2b8924f85cbb7d9518373c785847ab18141d
balakireva.elena <balakireva.elena@pronit.ru>
2025-01-10 16:00:51 +0300

    Финальный коммит перед сдачей

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import './App.css'
 import Calendar from './Calendar.jsx'
-//import moment from 'moment'
 import moment from 'moment/min/moment-with-locales'
 
 function App() {
-  //const now = new Date();
   
   moment.locale('ru');
   const now = moment();
-// внутри компонента App:
 return (
   <Calendar date={now} />
 );
--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -6,37 +6,8 @@ function getMonthPlural(date) {
     return monthX;
 }
 
-function getDaysArray(date) {
 
-    const startOfMonth = date.clone().startOf('month');
-
-    const startDay = startOfMonth.day();
-
-
-    let daysArray = Array(startDay);
-
-    const endOfPrevMonth = date.clone().subtract(1, 'months').endOf('month');
-    let endDate = endOfPrevMonth.date();
-
-    for (let i = startDay - 1; i > 0; i--) {
-        daysArray[i] = endDate--;
-    }
-    daysArray[startDay] = startOfMonth.date();
-
-    const daysInCurrentMonth = date.daysInMonth();
-
-    for (let i = 2; i < daysInCurrentMonth + 1; i++) {
-        daysArray.push(i);
-    }
-
-    let dayOfNextMonth = 1;
-    while ((daysArray.length - 1) % 7 != 0) {
-        daysArray.push(dayOfNextMonth++);
-    }
-    return daysArray;
-}
-
-function getStartWeek(date, startDay, endDate, daysCounter) {
+function getFirstWeek(startDay, endDate) {
 
     let prevDaysArray = Array(startDay - 1);
     for (let i = startDay - 2; i >= 0; i--) {
@@ -46,27 +17,32 @@ function getStartWeek(date, startDay, endDate, daysCounter) {
     let weekDaysArray = Array(7 - prevDaysArray.length);
     for (let i = 0; i < weekDaysArray.length; i++) {
         weekDaysArray[i] = i + 1;
-        daysCounter++;
     }
 
-    let today = -1;
-
-    if (weekDaysArray.includes(date.date())) {
-        today = date.date();
-    }
-
-    let nextMonthDays = [];
-    let week1 = { today: today, daysOfPrevMonth: prevDaysArray, days: weekDaysArray, daysOfNextMonth: nextMonthDays };
+    let week1 = { daysOfPrevMonth: prevDaysArray, days: weekDaysArray, daysOfNextMonth: [] };
     return week1;
 }
 
+function getLastWeek(date, daysInLastWeek, daysCounter){
+    let weekDaysArray = Array(daysInLastWeek);
+        let i = 0;
+        while (daysCounter < date.daysInMonth()) {
+            weekDaysArray[i++] = ++daysCounter;
+        }
+        let daysOfNextMonth = Array(7 - daysInLastWeek);
+        for (let i = 0; i < (7 - daysInLastWeek); i++) {
+            daysOfNextMonth[i] = i + 1;
+        }
+        let week = { daysOfPrevMonth: [], daysOfNextMonth: daysOfNextMonth, days: weekDaysArray };
+        return week;
+}
 
-function getDaysArray2(date) {
+function getDaysArray(date) {
     /*Хотим объект формата:
     const daysArray = [
-    { weekid: 1, dayOfPrevMonth: [30, 31], today: -1, days: [1, 2, 3], daysOfNextMonth:[] },
-    { weekid: 2, dayOfPrevMonth: [], today: 8, days: [1, 2, 3], daysOfNextMonth:[] },
-    { weekid: 3, dayOfPrevMonth: [, today: -1, days: [1, 2, 3], daysOfNextMonth:[1, 2, 3] }
+    { dayOfPrevMonth: [30, 31], days: [1, 2, 3], daysOfNextMonth:[] },
+    { dayOfPrevMonth: [], days: [1, 2, 3], daysOfNextMonth:[] },
+    { dayOfPrevMonth: [, days: [1, 2, 3], daysOfNextMonth:[1, 2, 3] }
   ];
     */
 
@@ -80,10 +56,8 @@ function getDaysArray2(date) {
     let endDate = endOfPrevMonth.date();
 
     let daysCounter = 0; //Счётчик, какое число мы добавили в массив
-    let weeksCounter = 0;
     if (daysCounter == 0 && startDay != 0) {
-        const week1 = getStartWeek(date, startDay, endDate, daysCounter);
-        week1.weekid = weeksCounter++;
+        const week1 = getFirstWeek(startDay, endDate);
         daysCounter = daysCounter + week1.days.length;
         daysArray.push(week1);
     }
@@ -93,29 +67,12 @@ function getDaysArray2(date) {
         for (let i = 0; i < 7; i++) {
             weekDaysArray[i] = ++daysCounter;
         }
-        let today = -1;
-        if (weekDaysArray.includes(date.date())) {
-            today = date.date();
-        }
-        let week = { weekid: weeksCounter++, today: today, daysOfPrevMonth: [], daysOfNextMonth: [], days: weekDaysArray };
+        let week = { daysOfPrevMonth: [], daysOfNextMonth: [], days: weekDaysArray };
         daysArray.push(week);
     }
     const daysInLastWeek = date.daysInMonth() - daysCounter;
-    if (daysInLastWeek < 7) {
-        let weekDaysArray = Array(daysInLastWeek);
-        let i = 0;
-        while (daysCounter < date.daysInMonth()) {
-            weekDaysArray[i++] = ++daysCounter;
-        }
-        let today = -1;
-        if (weekDaysArray.includes(date.date())) {
-            today = date.date();
-        }
-        let daysOfNextMonth = Array(7 - daysInLastWeek);
-        for (let i = 0; i < (7 - daysInLastWeek); i++) {
-            daysOfNextMonth[i] = i + 1;
-        }
-        let week = { weekid: weeksCounter++, today: today, daysOfPrevMonth: [], daysOfNextMonth: daysOfNextMonth, days: weekDaysArray };
+    if (daysInLastWeek > 0) {
+        let week = getLastWeek(date, daysInLastWeek, daysCounter);
         daysArray.push(week);
     }
 
@@ -132,34 +89,51 @@ function Calendar({ date }) {
 
     const monthX = getMonthPlural(date);
     const year = date.year();
-    //const daysArray = getDaysArray(date);
-    const daysArray = getDaysArray2(date);
+
+    const daysArray = getDaysArray(date);
 
     const listOfWeeks = Array(daysArray.length);
 
     for (let i = 0; i < listOfWeeks.length; i++) {
+        let DayKey = 0;
         let prev = [];
 
         if (daysArray[i].daysOfPrevMonth.length > 0) {
             prev = daysArray[i].daysOfPrevMonth.map((number1) =>
-                <td className="ui-datepicker-other-month">{number1}</td>
+                <td key = {DayKey++} className="ui-datepicker-other-month">{number1}</td>
             );
         }
-        let n = daysArray[i].days.map((number2) =>
-            <td>{number2}</td>
-        );
+
+
+        let n = [];
+        if (daysArray[i].days.includes(dayN)) {
+            for (let j = 0; j < daysArray[i].days.length; j++) {
+                if (daysArray[i].days[j] == dayN) {
+                    n = n.concat(<td className="ui-datepicker-today">{daysArray[i].days[j]}</td>);
+                }
+                else {
+                    n = n.concat(<td key = {DayKey++}>{daysArray[i].days[j]}</td>);
+                }
+            }
+        }
+        else {
+            n = daysArray[i].days.map((number2) =>
+                <td key = {DayKey++}>{number2}</td>
+            );
+        }
+
 
         let next = [];
         if (daysArray[i].daysOfNextMonth.length > 0) {
-            next = daysArray[i].daysOfNextMonth.map((number1) =>
-                <td className="ui-datepicker-other-month">{number1}</td>
+            next = daysArray[i].daysOfNextMonth.map((number3) =>
+                <td key = {DayKey++} className="ui-datepicker-other-month">{number3}</td>
             );
         }
 
         const listItems = prev.concat(n.concat(next));
-        listOfWeeks[i] = <tr>{listItems}</tr>;
+        listOfWeeks[i] = <tr key = {i}>{listItems}</tr>;
+        
     }
-
 
     return (
         <div className="ui-datepicker">
